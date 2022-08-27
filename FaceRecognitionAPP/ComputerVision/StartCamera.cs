775839b98239f6dfdf3ea610289eab62ab7470a5
Mason K <kirb5233@fredonia.edu>
2022-08-27 15:25:33 -0400

    adding windows app and wrapper for the C++ code

---
+++ FaceRecognitionAPP/ComputerVision/StartCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+
+namespace FaceRecognitionAPP.ComputerVision
+{
+    class StartCamera
+    {
+        public const string CppStartCameraDLL = @"\\10.2.63.252\testCV\FaceRecognitionAPP\bin\Debug\netcoreapp3.1\FaceRecognitionAPP.dll";
+
+        [DllImport(CppStartCameraDLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int TurnCameraOn();
+
+        public void turnCameraOn()
+        {
+            TurnCameraOn();
+        }
+    }
+}
--- FaceRecognitionAPP/MainWindow.xaml.cs
+++ FaceRecognitionAPP/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FaceRecognitionAPP.ComputerVision;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,14 @@ namespace FaceRecognitionAPP
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StartCamera camera = new StartCamera();
+
+            camera.turnCameraOn(); 
+
         }
     }
 }
